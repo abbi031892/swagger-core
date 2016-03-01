9ca108e5e5dadb5c25ec3116e6c4ea575f05a6dd
Himanshu Khurana <himanshu@local>
2016-03-01 14:34:25 +0000

    Instrumented by Insights Service for uploading the artifacts

--- df-build.sh
+++ df-build.sh
@@ -7,4 +7,4 @@ echo "Zipping artifacts begin..."
 zip libs.zip $(git ls-files -o | grep -e target/dependency/.*jar) || { echo "Error: Zipping libs failed"; exit 1; }
 zip build.zip $(git ls-files -o | grep -e target/classes/.*class) || { echo "Error: Zipping binaries failed"; exit 1; }
 echo "Zipping artifacts DONE!!!"
-# Modified by Insights Service at 2016-02-17 12:44:35.727531
+# Modified by Insights Service at 2016-03-01 14:33:44.252384
